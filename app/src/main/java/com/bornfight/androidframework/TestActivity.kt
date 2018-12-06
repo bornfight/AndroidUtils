bec183e27184edea485c940a87b42cfaebc5f7aa
Tomislav <smrecki.tomislav@gmail.com>
2018-12-06 15:52:23 +0100

    Gradle update

--- app/src/main/java/com/bornfight/androidframework/TestActivity.kt
+++ app/src/main/java/com/bornfight/androidframework/TestActivity.kt
@@ -1,12 +1,13 @@
 package com.bornfight.androidframework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
     }
 }
